f4ba766a8051f766da09ba7b31b630acd293c271
Jesufemi <oladapofemmy@gmail.com>
2024-07-25 09:40:10 +0100

    t

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -5,10 +5,39 @@ MRU cahcing
 from collections import OrderedDict
 
 
-BaseCaching = __import__("base_caching").BaseCaching()
+BaseCaching = __import__("base_caching").BaseCaching
 
 
-class MRUCaching(BaseCaching):
+class MRUCache(BaseCaching):
     """
     inherits from base class
     """
+    def __init__(self):
+        """
+        constructor
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        put into cache
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print(f"DISCARD: {mru_key}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        retrieves item
+        """
+        if  key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
